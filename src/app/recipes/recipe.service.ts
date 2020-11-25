76e22e6b67090d79ca4779c5ba5df7b0a1d93cf9
Azra <azra.becirevic@edu.fit.ba>
2020-11-25 12:15:28 +0100

    routes added

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,38 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+
+@Injectable()
+export class RecipeService{
+
+    recipeSelected = new EventEmitter<Recipe>();
+
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 
+        'This is simply a test', 
+        'https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg',
+        [
+            new Ingredient('Meat',1),
+            new Ingredient('French fries',20)
+        ]),
+    
+        new Recipe('A Test Recipe 2',
+         'This is simply a test 2', 
+        'https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg',
+        [
+            new Ingredient('Bread',2),
+            new Ingredient('Meat',1)
+        ])
+    ];
+
+    constructor(private shoppinglistService: ShoppingListService){}
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.shoppinglistService.addIngredients(ingredients);
+    }
+}
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';  //  EventEmitter, Output,
 import { Ingredient } from 'src/app/shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,9 +12,7 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('amountInput') amountInputRef: ElementRef;
  
 
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
@@ -21,7 +20,6 @@ export class ShoppingEditComponent implements OnInit {
   onAddIngredient(){
    
     const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value, this.amountInputRef.nativeElement.value);
-    this.ingredientAdded.emit(newIngredient); 
-    
+     this.shoppingListService.addIngredient(newIngredient);
   }
 }   
